ee1b8f819386f9a42d59225312e6054433097407
BLNorris <BLNorris@gmail.com>
2014-04-21 15:25:08 -0500

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Omawho::Application.routes.draw do
-  get "sessions/new"
-
-  get "users/new"
+  
+  get "logout" => "sessions#destroy", :as => "logout"
+  get "login" => "sessions#new", :as => "login"
+  get "signup" => "users#new", :as => "signup"
+  resources :users
+  resources :sessions
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
