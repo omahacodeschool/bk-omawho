d705deb48b80c9e36b89d15d18ae6cebba42cf16
Cara Heacock <caraheacock@gmail.com>
2014-04-21 14:51:42 -0500

    Rails app initial configurations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,9 @@
 Omawho::Application.configure do
+  config.logger = Logger.new(STDOUT)
+  config.logger.level = Logger.const_get(
+    ENV['LOG_LEVEL'] ? ENV['LOG_LEVEL'].upcase : 'DEBUG'
+  )
+  
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
