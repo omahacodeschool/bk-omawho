f2c7b49faa33844d0bf08300cef58dd81ee563ac
danielwells <danielwells.usnret@gmail.com>
2014-04-21 18:19:50 -0500

    Filled out the remainder of user.rb attr_accessible and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,33 @@ class User < ActiveRecord::Base
   authenticates_with_sorcery!
   
   attr_accessible :email, :password, :password_confirmation
-
+  attr_accessible :username, :first_name, :last_name, :tagline
+  attr_accessible :bio, :website, :company, :company_site
+  attr_accessible :facebook, :twitter, :pinterest, :linkedin, :github, :googleplus, :dribble, :instagram, :tumblr
+  # Note: security encryption and administrative status items not included in
+  #       attr_accessible lists
+  
   validates_confirmation_of :password
   validates_presence_of :password, :on => :create
   validates_presence_of :email
   validates_uniqueness_of :email
+  
+  validates_presence_of :username
+  validates_uniqueness_of :username
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  
+  validates :username, :format => { :with => /^(\w|-)*$/, :message => "can only contain letters, numbers, underscores, and dashes" }
+  validates :website, :format => { :with => /^http/ }, :allow_blank => true
+  validates :company_site, :format => { :with => /^http/ }, :allow_blank => true
+  validates_exclusion_of :username, :in => %w(category login logout add profile quiz beta)
+  validates_length_of :bio, :within => 0..140
+  validates_length_of :password, :within => 4..99
+  validates_length_of :tagline, :within => 0..30
+  
+  has_and_belongs_to_many :events
+  has_and_belongs_to_many :categories
+  has_many :images
+  has_many :embeds
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140421205510) do
+ActiveRecord::Schema.define(:version => 20140421214932) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                           :null => false
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(:version => 20140421205510) do
     t.string   "dribbble"
     t.string   "instagram"
     t.string   "tumblr"
+    t.string   "tagline"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
