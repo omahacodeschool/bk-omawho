0d9449ab836e916ed34fbcabbea25f2bf3d7dee2
Cara Heacock <caraheacock@gmail.com>
2014-04-21 14:16:45 -0500

    Created Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omawho::Application.config.secret_token = 'bf5d32fc531b5d55339521f9d6728e1293e64fb586bdc4fb93412d3600eea2059101169a8b9c940afe9e695478e061d6a11d0b6b4968aeefbd98c5611317ad71'
