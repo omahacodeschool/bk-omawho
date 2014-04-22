a53615aaee6338f74dd2d77e41a7cec72e2ae462
danielwells <danielwells.usnret@gmail.com>
2014-04-21 19:48:39 -0500

    Added relationships and validations to Embed model

--- app/models/embed.rb
+++ app/models/embed.rb
@@ -1,3 +1,9 @@
 class Embed < ActiveRecord::Base
   attr_accessible :description, :link, :type, :user_id
+  
+  belongs_to :user
+  
+  validates_presence_of :link
+  validates_inclusion_of :type, :in => %w(video audio), :message => "specified embed type not recognized"
+  
 end
