654f20a2ad588787c2e3d971c451151acba4ef11
danielwells <danielwells.usnret@gmail.com>
2014-04-21 19:30:55 -0500

    Added relationships, validations, scopes, and method to Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,21 @@
 class Event < ActiveRecord::Base
-  attr_accessible :cancelled, :description, :end_time, :link, :location, :name, :start_time, :venue
+  attr_accessible :description, :link, :location, :name, :venue
+  attr_accessible :cancelled, :end_time, :start_time
+  
+  has_and_belongs_to_many :users
+  
+  # scope :approved, where(:approved => true)
+  scope :cancelled, where(:cancelled => true)
+  scope :not_cancelled, where(:cancelled => false)
+  
+  validates_presence_of :start_time
+  validates_presence_of :location
+  validates_presence_of :name
+
+  def is_current
+    self.start_time > Time.now
+  end
+  
 end
+
+
