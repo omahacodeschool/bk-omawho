4881c5f46a6d6d65f1ef9bf734c8fb3d25bb1e7f
danielwells <danielwells.usnret@gmail.com>
2014-04-21 19:00:11 -0500

    Created join table for categories_users

---
+++ db/migrate/20140421235851_join_table_for_categories_users.rb
@@ -0,0 +1,12 @@
+class JoinTableForCategoriesUsers < ActiveRecord::Migration
+  def up
+    create_table :categories_users, :id => false do |t|
+      t.integer :category_id
+      t.integer :user_id
+    end
+  end
+
+  def down
+    drop_table: :categories_users
+  end
+end
