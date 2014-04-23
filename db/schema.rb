82c78325108e308c7697ada2f562c644bae140f5
BLNorris <BLNorris@gmail.com>
2014-04-23 17:54:44 -0500

    removed profile picture from images

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140423211215) do
+ActiveRecord::Schema.define(:version => 20140423225239) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -57,9 +57,8 @@ ActiveRecord::Schema.define(:version => 20140423211215) do
     t.string   "file"
     t.integer  "user_id"
     t.string   "description"
-    t.boolean  "profile_picture"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "users", :force => true do |t|
