3c5a898a92e2fd17bee56acb2192b627c5fa2a79
BLNorris <BLNorris@gmail.com>
2014-04-24 15:00:07 -0500

    scraping spn site functioning, needs a new setup for each new site added

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -4,16 +4,50 @@ class Scraper
     
   end
   
-  def scrape_events()
+  def self.scrape_spn()
     
     page = Nokogiri::HTML(open("http://www.siliconprairienews.com/events")) 
     
+    #each section containing an event
     page.css(".vevent").each do |e|
       event = Event.new
       
-      #event.title = 
-      puts e.css("td.event-name a")
+      #event.name
+      #puts e.css("td.event-name a")[0].text
+      event.name = e.css("td.event-name a")[0].text
+      #event.description
       
+      #event.start_time
+      #puts e.css("td.event-date span.dtstart span.value-title")[0]["title"]
+      if e.css("td.event-date span.dtstart span.value-title")[0]
+        event.start_time = e.css("td.event-date span.dtstart span.value-title")[0]["title"]
+      end
+      
+      #event.end_time
+      #puts e.css("td.event-date span.dtstart span.dtend span.value-title")[0]["title"]
+      if e.css("td.event-date span.dtend span.value-title")[0]
+        event.end_time = e.css("td.event-date span.dtend span.value-title")[0]["title"]
+      end
+      
+      #event.venue
+      if e.css("td.location.vcard").text.strip == "TBA"
+        #puts e.css("td.location.vcard").text.strip
+        event.venue = "TBA"
+        event.location = "TBA"
+      else
+        #puts e.css("td.location.vcard").text.strip.split("\n")[0]
+        event.venue = e.css("td.location.vcard").text.strip.split("\n")[0]
+        #event.location
+        l = e.css("td.location.vcard span.fn.org span span.adr span.locality").text
+        l += e.css("td.location.vcard span.fn.org span span.adr span.region").text
+        event.location = l
+      end
+      
+      #event.link
+      #puts e.css("td.event-name a")[0]["href"]
+      event.link = e.css("td.event-name a")[0]["href"]
+      event.approved = true
+      event.save
     end
   end
 end
