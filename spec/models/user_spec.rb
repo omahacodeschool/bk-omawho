a19858a82ac1af1b8896c47c95d16cbfdd85b304
danielwells <danielwells.usnret@gmail.com>
2014-04-26 17:23:35 -0500

    Completed user search tests for multiple query configurations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,9 +22,25 @@ describe User do
     expect(User.search_name("Ob pLaCE").count).to eq(3)
   end
     
-  it "finds user by fuzzy search on first name" do
+  it "finds users by fuzzy search on first name" do
     target_user = User.find_by_first_name("Bob")
     expect(User.search_name("Ob")).to include(target_user)
+  end
+  
+  it "finds users by fuzzy search on last name" do
+    target_user = User.find_by_last_name("Wells")
+    expect(User.search_name("WEl")).to include(target_user)
+  end
+  
+  it "finds users by fuzzy search on company name" do
+    target_user = User.find_by_company("Flywheel")
+    expect(User.search_name("Fly whe")).to include(target_user)
+  end  
+  
+  it "find users by fuzzy search with long query string" do
+    target_user1 = User.find_by_last_name("Smith")
+    target_user2 = User.find_by_company("Some Place")
+    expect(User.search_name("works at Some Place with SMit as name")).to include(target_user1, target_user2)
   end
   
 end
