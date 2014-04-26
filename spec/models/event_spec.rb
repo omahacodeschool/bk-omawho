4e2e5538fc69e57d2f8839b8105534e1a7c1aa7d
BLNorris <BLNorris@gmail.com>
2014-04-26 17:13:43 -0500

    updated tests

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Event do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "can make a new event" do
+    @event = Event.create(name: "Omaha Code School Graduation", venue: "Omaha Code School", location: "Suite #4107\n200 S 31st Ave\nOmaha, Nebraska", start_time: @now, end_time: @now)
+    expect(@event.name).to eq("Omaha Code School Graduation")
+  end
+  
+  it "fails on an event that doesnt have a start time" do
+    @event = Event.create(name: "Omaha Code School Graduation", venue: "Omaha Code School", location: "Suite #4107\n200 S 31st Ave\nOmaha, Nebraska")
+    expect(@event.name).to eq("Omaha Code School Graduation")
+  end
+  
+  
 end
